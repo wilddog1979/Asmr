edba68c7aaf63cb647319e334cc119407823871f
Ferenc Kemeny <ferenc.kemeny@exxonmobil.com>
2023-07-31 16:52:05 +0200

    project restructuring

--- Z80Assembler/src/test/java/org/eaSTars/asm/ast/test/AssemblerLineTester.java
+++ Z80Assembler/src/test/java/org/eaSTars/asm/ast/test/AssemblerLineTester.java
@@ -1,17 +1,18 @@
 package org.eaSTars.asm.ast.test;
 
-import static org.junit.Assert.assertEquals;
 
 import org.eaSTars.asm.AbstractTester;
 import org.eaSTars.asm.ast.AssemblerLine;
 import org.eaSTars.asm.ast.CompilationUnit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public abstract class AssemblerLineTester extends AbstractTester {
 
 	protected AssemblerLine invokeParser(String content) {
 		CompilationUnit compilationUnit = getCompilationUnit(content);
-		
-		assertEquals("One line expected", 1, compilationUnit.getLineCount());
+
+		assertEquals(1, compilationUnit.getLineCount(), "One line expected");
 
 		return compilationUnit.getLine(0);
 	}
